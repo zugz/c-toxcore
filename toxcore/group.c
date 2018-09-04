df1863fd70e5f6c17890fdfe5be8ee01c4c6345f
zugz (tox) <mbays+tox@sdf.org>
2018-09-04 20:51:22 +0200

    remove redundant comparisons after reallocs

--- toxcore/group.c
+++ toxcore/group.c
@@ -509,7 +509,7 @@ static int note_peer_active(Group_Chats *g_c, uint32_t groupnumber, uint16_t pee
 
         Group_Peer *frozen_temp = (Group_Peer *)realloc(g->frozen, sizeof(Group_Peer) * (g->numfrozen));
 
-        if (frozen_temp == nullptr && g->numfrozen > 0) {
+        if (frozen_temp == nullptr) {
             return -1;
         }
 
@@ -684,7 +684,7 @@ static int delpeer(Group_Chats *g_c, uint32_t groupnumber, int peer_index, void 
 
         Group_Peer *temp = (Group_Peer *)realloc(g->group, sizeof(Group_Peer) * (g->numpeers));
 
-        if (temp == nullptr && g->numpeers > 0) {
+        if (temp == nullptr) {
             return -1;
         }
 
